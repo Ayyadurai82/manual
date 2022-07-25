bb8cff83affba90ec97a5aa0f0abaac819d1730a
Ayyadurai82 <duraiece40@gmail.com>
2022-07-25 16:32:54 +0530

    Ayya

---
+++ FirstGit.java
@@ -0,0 +1,19 @@
+package org.manual;
+
+public class FirstGit {
+	public static void main(String[] args) {
+		
+
+
+	System.out.println(1);
+	System.out.println(11);
+	System.out.println(21);
+	System.out.println(31);
+	System.out.println(41);
+	
+	
+	
+	}	
+	
+	
+}
